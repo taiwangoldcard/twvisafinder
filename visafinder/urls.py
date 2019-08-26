308642651ed019b26e372041cc595a90d4aa1628
Tom Fifield <tom@tomfifield.net>
2019-08-26 17:21:26 +0800

    Add urls, update homepage
    Adds urls for views-to-be and implements basic visa detail view.
    
    Updates the homepage to display visa categories, and adds a bit of css and js
    to support it.
    
    Update the footer disclaimer, as we now have content from CC-BY and other
    sources.

--- visafinder/urls.py
+++ visafinder/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^visa/(?P<visa>\w+)/$', views.visa, name='visa'),
+    url(r'^(?P<visa_category>\w+)/$', views.category, name='category'),
 ]
--- visafinder/views.py
+++ visafinder/views.py
@@ -3,22 +3,37 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.shortcuts import render
-
+from visafinder.models import VisaCategory
+from visafinder.models import Visa
 
 def home(request):
     # show the list of visa categories
-    return render(request, 'home.html', context={'support_email': settings.SUPPORT_EMAIL })
+    return render(request, 'home.html', context={
+        'support_email': settings.SUPPORT_EMAIL,
+        'categories': VisaCategory.objects.all(),
+        })
 
-def category(request):
+def category(request, visa_category):
     # show results within a category, or a form with additional information to get a results
-    return render(request, 'home.html', context={'support_email': settings.SUPPORT_EMAIL })
+    return render(request, 'category.html', context={'support_email': settings.SUPPORT_EMAIL })
 
 def result(request):
     # show personalised results based on the input
-    return render(request, 'home.html', context={'support_email': settings.SUPPORT_EMAIL })
+    return render(request, 'result.html', context={'support_email': settings.SUPPORT_EMAIL })
 
-def visa(request):
+def visa(request, visa):
     # show the information we have about a particular visa
-    return render(request, 'visa.html', context={'support_email': settings.SUPPORT_EMAIL })
-
+    print(visa)
+    matched_visas = Visa.objects.filter(name=visa)
+    print(matched_visas)
+    if len(matched_visas) == 1:
+        return render(request, 'visa.html', context={
+            'support_email': settings.SUPPORT_EMAIL,
+            'visa': matched_visas[0],
+             })
+    else:
+        return render(request, 'home.html', context={
+            'support_email': settings.SUPPORT_EMAIL,
+            'categories': VisaCategory.objects.all(),
+            })
 

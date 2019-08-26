018be05caf22c7419ac19d29f133e4a15fd04f52
Tom Fifield <tom@tomfifield.net>
2019-08-26 17:39:41 +0800

    Add initial category view
    Adds an initial category view that only lists visas in that category.
    Updates URL parsing to facilitate current categories.

--- visafinder/views.py
+++ visafinder/views.py
@@ -15,7 +15,19 @@ def home(request):
 
 def category(request, visa_category):
     # show results within a category, or a form with additional information to get a results
-    return render(request, 'category.html', context={'support_email': settings.SUPPORT_EMAIL })
+    matched_categories = VisaCategory.objects.filter(name=visa_category)
+    if len(matched_categories) == 1:
+        visas = matched_categories[0].visa_set.all()
+        return render(request, 'category.html', context={
+            'support_email': settings.SUPPORT_EMAIL,
+            'category': matched_categories[0].name,
+            'visas': visas,
+             })
+    else:
+        return render(request, 'home.html', context={
+            'support_email': settings.SUPPORT_EMAIL,
+            'categories': VisaCategory.objects.all(),
+            })
 
 def result(request):
     # show personalised results based on the input
@@ -24,8 +36,8 @@ def result(request):
 def visa(request, visa):
     # show the information we have about a particular visa
     print(visa)
+    print(Visa.objects.all())
     matched_visas = Visa.objects.filter(name=visa)
-    print(matched_visas)
     if len(matched_visas) == 1:
         return render(request, 'visa.html', context={
             'support_email': settings.SUPPORT_EMAIL,

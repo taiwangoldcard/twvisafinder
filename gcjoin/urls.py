d2d1183c18e53c5df7df2bcf17e48a4a86344218
Tom Fifield <tom@tomfifield.net>
2022-05-09 20:38:37 +0800

    Update to Django4
    The primary changes are in the URL paths, code seems to be functionally
    OK.

--- gcjoin/urls.py
+++ gcjoin/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.join, name='join'),
+    path('', views.join, name='join'),
 ]
--- visafinder/urls.py
+++ visafinder/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^gold-card-qualification/$', views.goldcardqualification, name='gold-card-qualification'),
-    url(r'^gold-card-qualification/(?P<tree_id>[0-9]+)/$', views.goldcardqualificationtree, name='gold-card-qualification-tree'),
-    url(r'^gold-card-qualification/results/$', views.goldcardqualificationresults, name='gold-card-qualification-results'),
-    url(r'^visa/(?P<visa>[\w|\W]+)/$', views.visa, name='visa'),
-    url(r'^(?P<visa_category>[\w|\W]+)/$', views.category, name='category'),
+    path('', views.home, name='home'),
+    path('gold-card-qualification/', views.goldcardqualification, name='gold-card-qualification'),
+    re_path(r'^gold-card-qualification/(?P<tree_id>[0-9]+)/$', views.goldcardqualificationtree, name='gold-card-qualification-tree'),
+    path('gold-card-qualification/results/', views.goldcardqualificationresults, name='gold-card-qualification-results'),
+    re_path(r'^visa/(?P<visa>[\w|\W]+)/$', views.visa, name='visa'),
+    re_path(r'^(?P<visa_category>[\w|\W]+)/$', views.category, name='category'),
 ]
